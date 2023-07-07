3140498e96eb3c05db72727c6dec776fe1747c02
Kolechenko Galina <joannahailey@mail.ru>
2023-07-07 15:15:49 +0300

    first commit

---
+++ app/src/main/java/com/example/jetpack_compose_app/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.jetpack_compose_app
+
+import android.app.ActionBar
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposableTarget
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jetpack_compose_app.ui.theme.Jetpack_compose_appTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Jetpack_compose_appTheme {
+                var name by remember {
+                    mutableStateOf("")
+                }
+                var names by remember{
+                    mutableStateOf(listOf<String>())
+                }
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                        Row(modifier = Modifier.fillMaxWidth()) {
+                            OutlinedTextField(
+                                value = name,
+                                onValueChange = {text ->
+                                    name = text
+                                },
+                                modifier = Modifier.weight(1f)
+                            )
+                            Spacer(modifier = Modifier.width(20.dp))
+                             Button(onClick = {
+                                 if(name.isNotBlank())
+                                 names = names + name
+                                 name = ""
+                             }) {
+                                Text(text = "Add")
+                             }
+                        }
+                    nameList(names)
+
+
+                }
+
+            }
+        }
+    }
+    @Composable
+    fun nameList(names : List<String>, modifier: Modifier = Modifier){
+        LazyColumn() {
+
+            items(names){
+                    currentName ->
+                Text(text = currentName,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp))
+                Divider()
+            }
+        }
+    }
+
+}
